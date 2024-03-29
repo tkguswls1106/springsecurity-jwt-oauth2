98e808067d6ca1ccd5749fc2bc37d72dcd8aa3a6
sahyunjin <tkguswls1106@naver.com>
2024-03-30 02:38:43 +0900

    refreshToken까지 사용한 oAuth2 버전 / 로그인 및 회원가입 과정 설명 작성

--- src/main/java/com/shj/springboot3/controller/AuthController.java
+++ src/main/java/com/shj/springboot3/controller/AuthController.java
@@ -37,14 +37,20 @@ public class AuthController {
 
     private final AuthService authService;
 
+    // < OAuth2 로그인 및 회원가입(추가정보입력) 로직 과정 >
+    // - 과정 1. 처음 OAuth 소셜 로그인이 성공한다면, 일단 프론트에서 헤더에 Access 토큰을 지니게한뒤에, OAuth2LoginSuccessHandler에서 반환되는 ResponseEntity 내부의 isNewUser 필드의 boolean 결과에 따라 판단함.
+    // - 과정 true-1. 만약 isNewUser=true인 경우, 프론트에서 추가정보 입력을 위한 회원가입 페이지로 안내함. 그리고 입력한 정보를 백엔드로 "/oauth2/signup" 경로로 api 요청을 보냄.
+    // - 과정 true-2. 백엔드에서 회원가입 절차 성공 시, 권한이 ROLE_GUEST->ROLE_USER로 변경된 새로운 Access 토큰에 대한 TokenDto(리프레시 토큰은 변함X)을 프론트로 반환해줌. 프론트에서는 이 토큰을 헤더에 지니게함.
+    // - 과정 true-3. 이제 프론트에서 원하는 메인 페이지로 이동해서 서비스를 이용하면 됨.
+    // - 과정 false-1. 만약 isNewUser=false인 경우, 이미 기존 회원이므로 추가정보 입력을 위한 회원가입 절차 없이, 바로 프론트에서 원하는 메인 페이지로 이동해서 서비스를 이용하면 됨.
 
-    // - oauth2signup()으로 해당 성공 ResponseEntity를 반환에 성공한다면, 프론트에서는 이미 헤더에 저장해둔 토큰으로 메인 페이지로 이동시키면됨.
     // 문제점 1. 프론트에서 <a href="/oauth2/authorization/kakao"> 를 어떻게 구현할것인가?
     // (O) 문제점 2. 토큰 만료의 경우를 TokenProvider에서 말고 다른곳에서 처리하는 법은? (JwtAuthenticationEntryPoint 대신 JwtExceptionFilter 로 가능할지도)
     // (O) 문제점 3. 토큰 만료의 경우를 걸러냈다치고, 이럴때 리프레쉬 토큰을 재설정해서 프론트에 넘겨주는 법은?
     // (O) 문제점 4. '/oauth2/signup'는 Role.GUEST만, 나머지 api url은 전부 Role.USER 또는 Role.ADMIN 만 사용가능하도록, .requestMatchers 설정하는 법은?
-    // 문제점 5. login api와 signup api의 구분은 정확히 어떻게 할것이며 그러한 플로우는 어떻게 진행할것인가?
+    // (O) 문제점 5. login api와 signup api의 구분은 정확히 어떻게 할것이며 그러한 플로우는 어떻게 진행할것인가?
     // 문제점 6. '.requestMatchers("/**").permitAll()' 이거 없이 어떻게 초반 oauth 로그인을 진행할것인가?
+    // 문제점 7. Refresh Token 관련 로직을 마저 구현 완료할것.
     @PostMapping("/oauth2/signup")  // 이 api는 헤더에 JWT토큰이 반드시 필요하다.
     public ResponseEntity signup(Authentication authentication, @RequestBody UserSignupRequestDto userSignupRequestDto) {  // 여기서 Role을 USER로 교체해주지 않으면 다른 로그인 필수 api를 사용하지 못한다.
         SignupResponseDto signupResponseDto = authService.signup(userSignupRequestDto);
